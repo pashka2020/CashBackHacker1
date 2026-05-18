abca6bc0a688dc256b62d706a4456166c67f7f81
Neto test <pashtet201807@gmail.com>
2026-05-18 16:35:51 +0300

    add TestNG tests and find a bug

--- src/test/java/CashbackHack1ServiceTest.java
+++ src/test/java/CashbackHack1ServiceTest.java
@@ -1,2 +1,30 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackHack1ServiceTest {
+
+    @Test
+    public void shouldCalculateRemainIfLessThan1000() {
+        CashbackHack1Service service = new CashbackHack1Service();
+
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfExact1000() {
+        CashbackHack1Service service = new CashbackHack1Service();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+    }
 }
